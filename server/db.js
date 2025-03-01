f55f4a7811b0cfb9b82ff1eb278be3811b7b951c
Thunder117 <christopherlogan2000@outlook.com>
2025-02-28 21:18:35 -0500

    added mongodb

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:qZXegC5gDyquXf8ds1YGBwyN@172.21.77.121:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
